0531e96cd23323f5b8a1648ac256e29d83d2f941
Nikolaev Valentin <val8673@gmail.com>
2022-12-17 16:14:14 +0600

    Add docker-compose

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import os
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# os.environ['DATABASE_URL']
+
 class Config:
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///avito.db'
+	# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/postgres'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SQLALCHEMY_ECHO = True
 	DEBUG = True
